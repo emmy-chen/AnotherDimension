cc5b162f64a5af955105f2974c64ad5ff70bcad1
emmy-chen <echen20@stuy.edu>
2020-10-27 09:31:12 -0400

    adding sum() method for double arrays

--- ArrayOps.java
+++ ArrayOps.java
@@ -44,9 +44,15 @@ public class ArrayOps {
     return a;
   }
 
-//  public static  int sum(int[][] arr) {
-
-  //}
+  public static  int sum(int[][] arr) {
+    int sum = 0;
+    for(int i = 0; i < arr.length; i++) {
+      for(int j = 0; j < arr[0].length; j++) {
+        sum += arr[i][j];
+      }
+    }
+    return sum;
+  }
 
 //  public static int[] sumCols(int[][] matrix) {
 
