36b18db6fe0f7b28c6dfa93f47c0a652bd352f0c
emmy-chen <echen20@stuy.edu>
2020-10-27 09:01:50 -0400

    adding sum() method

---
+++ ArrayOps.java
@@ -0,0 +1,43 @@
+public class ArrayOps {
+  public static int sum(int[] arr) {
+    if (arr.length == 0) {
+      return 0;
+    }
+    int summ = 0;
+    for (int i = 0; i < arr.length; i++) {
+      summ += arr[i];
+    }
+    return summ;
+  }
+//  public static  int largest(int[]arr) {
+
+  //}
+
+//  public static int[] sumRows(int[][] matrix) {
+
+  //}
+
+//  public static  int[] largestInRows(int[][] matrix) {
+
+  //}
+
+//  public static  int sum(int[][] arr) {
+
+  //}
+
+//  public static int[] sumCols(int[][] matrix) {
+
+  //}
+
+//  public static boolean isRowMagic(int[][] matrix) {
+
+  //}
+
+//  public static boolean isColMagic(int[][] matrix) {
+
+  //}
+
+//  public static boolean isLocationMagic(int[][] matrix, int row, int col) {
+
+  //}
+}
